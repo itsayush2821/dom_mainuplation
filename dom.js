ba3c54f31bb5eab4bb82738b418cc62ec1372e45
itsayush2821 <annuj0028@gmail.com>
2023-04-26 01:28:05 +0530

    modifying_dom

--- dom.js
+++ dom.js
@@ -33,17 +33,88 @@
 // console.log(li)
 // li[4].style.fontWeight="bold"
 
-var secondEle = document.querySelector(".list-group-item:nth-child(2)");
-secondEle.style.backgroundColor="green"
+// var secondEle = document.querySelector(".list-group-item:nth-child(2)");
+// secondEle.style.backgroundColor="green"
 
-var thirdEle = document.querySelector(".list-group-item:nth-child(3)");
-thirdEle.style.display="none"
+// var thirdEle = document.querySelector(".list-group-item:nth-child(3)");
+// thirdEle.style.display="none"
 
-var secondElement = document.querySelectorAll(".list-group-item")
-secondElement[1].style.color="green"
+// var secondElement = document.querySelectorAll(".list-group-item")
+// secondElement[1].style.color="green"
 
-var odd = document.querySelectorAll("li:nth-child(odd)");
+// var odd = document.querySelectorAll("li:nth-child(odd)");
 
-for(let i=0;i<odd.length;i++){
-    odd[i].style.backgroundColor = "green"
-}
+// for(let i=0;i<odd.length;i++){
+//     odd[i].style.backgroundColor = "green"
+// }
+
+//Traversing the dom
+
+
+//ParentNode
+// var itemList = document.querySelector("#items")
+//     console.log(itemList.parentNode)
+//     itemList.parentNode.style.backgroundColor = "grey"
+
+
+//ParentElement
+
+var itemList = document.querySelector("#items")
+    // console.log(itemList.parentElement)
+    // itemList.parentElement.style.backgroundColor = "grey"
+
+    // console.log(itemList.childNodes)
+
+    // console.log(itemList.children)
+
+    // console.log(itemList.children[1])
+
+    // itemList.children[1].style.color = "yellow"
+
+    // console.log(itemList.firstChild)
+
+    // console.log(itemList.firstElementChild)
+
+    // itemList.firstElementChild.textContent="Ayush"
+
+
+   //LastChild 
+    // console.log(itemList.lastChild)
+
+    // console.log(itemList.lastElementChild)
+
+    // itemList.lastElementChild.textContent="Ayush"
+
+    // console.log(itemList.nextSibling)
+
+    // console.log(itemList.nextElementSibling)
+
+    // console.log(itemList.previousSibling)
+
+    // console.log(itemList.previousElementSibling)
+
+    // itemList.previousElementSibling.style.color="red"
+
+    //createElement
+
+    // var newDiv = document.createElement('div')
+
+    // newDiv.className='hello'
+    // newDiv.id="hello1"
+    // newDiv.setAttribute('title' , 'Hello Div')
+    
+
+    // let newDivText = document.createTextNode("Hellow World")
+
+    // newDiv.appendChild(newDivText)
+
+    // console.log(newDiv)
+
+     var container = document.querySelector(".container");
+     var ele = document.createElement("h1")
+     ele.appendChild(document.createTextNode("Hello World"))
+    //  container.firstChild = ele
+    container.insertBefore(ele , document.querySelector('#header-title'))
+    //  container.appendChild(ele)
+
+    itemList.insertBefore(ele , itemList.firstElementChild)
