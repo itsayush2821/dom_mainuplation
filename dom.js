76af58f7a9245034c5595dec99802c410c142adc
itsayush2821 <annuj0028@gmail.com>
2023-04-25 21:23:02 +0530

    QuerySelectorAll

--- dom.js
+++ dom.js
@@ -26,9 +26,24 @@
 // }
 
 
-var newElement = document.getElementsByClassName("list-group-items")
-newElement[0].style.backgroundColor="green";
+// var newElement = document.getElementsByClassName("list-group-items")
+// newElement[0].style.backgroundColor="green";
 
-var li = document.getElementsByTagName("li")
-console.log(li)
-li[4].style.fontWeight="bold"
+// var li = document.getElementsByTagName("li")
+// console.log(li)
+// li[4].style.fontWeight="bold"
+
+var secondEle = document.querySelector(".list-group-item:nth-child(2)");
+secondEle.style.backgroundColor="green"
+
+var thirdEle = document.querySelector(".list-group-item:nth-child(3)");
+thirdEle.style.display="none"
+
+var secondElement = document.querySelectorAll(".list-group-item")
+secondElement[1].style.color="green"
+
+var odd = document.querySelectorAll("li:nth-child(odd)");
+
+for(let i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor = "green"
+}
