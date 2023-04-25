eb7bc0a0a85cd6bc97ce8e29073e1c0bfa9932ce
itsayush2821 <annuj0028@gmail.com>
2023-04-25 16:35:32 +0530

    getElementsbyTagName

--- dom.js
+++ dom.js
@@ -1,27 +1,34 @@
-console.log(document.head)
-console.log(document.url)
-console.log(document.links)
-console.log(document.form)
-// console.log(document.form[0])
-console.log(document.images)
-console.log(document.all)
-var headerTitle =  (document.getElementById("header-title"))
-console.log(headerTitle)
-var header =  (document.getElementById("main-header"))
-document.title = "Change Item"
+// console.log(document.head)
+// console.log(document.url)
+// console.log(document.links)
+// console.log(document.form)
+// // console.log(document.form[0])
+// console.log(document.images)
+// console.log(document.all)
+// var headerTitle =  (document.getElementById("header-title"))
+// console.log(headerTitle)
+// var header =  (document.getElementById("main-header"))
+// document.title = "Change Item"
 
-header.style.border = "solid 2px black";
+// header.style.border = "solid 2px black";
 
-var addItem = document.getElementsByClassName("title-one");
-console.log(addItem)
-addItem[0].style.color = "green"
-// addItem.style.color="green"
-addItem[0].style.weight="bold"
+// var addItem = document.getElementsByClassName("title-one");
+// console.log(addItem)
+// addItem[0].style.color = "green"
+// // addItem.style.color="green"
+// addItem[0].style.weight="bold"
 
-var listGroup = document.getElementsByClassName("list-group-item")
+// var listGroup = document.getElementsByClassName("list-group-item")
 
-listGroup[2].style.backgroundColor="green"
-for(let i=0;i<listGroup.length;i++){
-    listGroup[i].style.fontWeight = "bold"
-}
+// listGroup[2].style.backgroundColor="green"
+// for(let i=0;i<listGroup.length;i++){
+//     listGroup[i].style.fontWeight = "bold"
+// }
 
+
+var newElement = document.getElementsByClassName("list-group-items")
+newElement[0].style.backgroundColor="green";
+
+var li = document.getElementsByTagName("li")
+console.log(li)
+li[4].style.fontWeight="bold"
